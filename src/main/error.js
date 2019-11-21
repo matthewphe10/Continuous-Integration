e4fcadd1f928f7ce060b5523669f5a49c0982afe
Matthew Phelan <matthew.phelan@uky.edu>
2019-11-21 15:03:33 -0500

    idk

---
+++ src/main/error.js
@@ -0,0 +1,15 @@
+
+var express = require('express')
+var app = express()
+
+// The code below will display 'Hello World!' to the browser when you go to http://localhost:3000
+app.get('/403', (req, res) => {
+    // the .status(200) isn't necessary since it is the default return status
+    res.status(403).send('not sure what happened')
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on http://localhost:3000')
+})
+
+module.exports = app;
---
+++ src/test/cases/error_test.js
@@ -0,0 +1,18 @@
+const {expect} = require('../chai')
+const request = require('supertest')
+
+const app = require('../../main/app')
+
+describe('GET - /', async () => {
+
+    it('returns 403', async () => {
+        // make a GET request on the app for the url '/'
+        const response = await request(app).get('/')
+
+        // check the HTTP status code
+        expect(response.statusCode).to.equal(403)
+        // check the raw text returned
+        expect(response.text).to.equal('not sure what happened')
+    })
+
+})
